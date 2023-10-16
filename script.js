dbdb9d86cbc1c37d5f20f801fbafe7e37506e70c
jestinjohnson <104827620+jestinjohnson@users.noreply.github.com>
2023-10-16 17:12:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+
+
+$(document).ready(function(){
+    // Add click event handlers to the filter buttons
+    $('#portfolio-flters li').click(function(){
+        // Remove the 'filter-active' class from all buttons
+        $('#portfolio-flters li').removeClass('filter-active');
+        
+        // Add the 'filter-active' class to the clicked button
+        $(this).addClass('filter-active');
+        
+        // Get the category to filter
+        var filterValue = $(this).attr('data-filter');
+        
+        // If the category is '*', show all items
+        if (filterValue == '*') {
+            $('.portfolio-item').show();
+        } else {
+            // Otherwise, hide all items and show only those with the selected category
+            $('.portfolio-item').hide();
+            $(filterValue).show();
+        }
+    });
+});
+
+
+
+
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
+
+  
+
+  document.addEventListener('DOMContentLoaded', function() {
+    AOS.init();
+  });
